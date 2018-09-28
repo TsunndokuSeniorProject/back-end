3669c134059b9ac593ef57a460a091e7ed409aee
jab <jibbizjab@gmail.com>
2018-09-29 00:56:28 +0700

    create ml model component and integrate to webserver

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ This back-end web server for Tsunndoku project.
 It's using python Flask as a server.
 # Dependencies
 To install, this should be done by using command pip install < module >
-* Flask==1.0.2
+* Python < 2.7.* >
+* Flask
+* NumPy
+* SciPy
+* Pandas
+* scikit-learn
 # Start Server
 * $ cd <inside the directory>
 * $ python server.py
@@ -11,5 +16,7 @@ To install, this should be done by using command pip install < module >
 ### /api/1.0/test
 This endpoint is using just for test. It provide a simple example of RestAPI concept.
 Inside the code there is a temporary database which allow to [POST] or [GET] by sending a request.
+### /api/1.0/predict
+This end point is allow to post { height, weight }, the server will predict a gender of the given information.
 # Upcoming
 #### ...
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 from flask import Flask, jsonify, request
+from model.model import Model
+
 app = Flask(__name__)
 
 temp_db_mutant = [{
     "id":"weapon-X",
     "name":"Logan Paul"
+}]
+temp_db_predict = [{
+    "id":1,
+    "height":68,
+    "weight":97,
+    "predicted_gender": "ApacheHelicopter",
 }]
 
 @app.route('/api/1.0/test', methods=['GET'])
@@ -22,5 +30,34 @@ def post_mutant():
     temp_db_mutant.append(req)
     return jsonify(temp_db_mutant)
 
+@app.route("/api/1.0/predict", methods=['GET'])
+def get_gender_predict():
+    return jsonify(temp_db_predict)
+
+@app.route("/api/1.0/predict/<int:id>", methods=['GET'])
+def get_gender_predict_id(id):
+    id = id-1
+    if id > len(temp_db_predict):
+        return "not found or invalid id",400
+    else:
+        return jsonify(temp_db_predict[id])
+
+@app.route("/api/1.0/predict", methods=['POST'])
+def post_gender_predict():
+    req = request.json
+    if not req or not 'height' in req or not 'weight' in req:
+        return "something missing",400
+    predit_result = model.predict([[req['height'], req['weight']]])
+    temp_db_predict.append({
+        "id":len(temp_db_predict)+1,
+        "height":req['height'],
+        "weight":req['weight'],
+        "predicted_gender":predit_result[0]
+    })
+    return jsonify(temp_db_predict)
+
 if __name__=="__main__":
+    model = Model().loadModelState('model/state/model_state.sav')
+
     app.run(port="5001")
+
