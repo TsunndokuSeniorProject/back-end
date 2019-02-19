c8571e42b27543f545700d5ffa6ecdf8031e109d
Kan Kamalanon <kan86197@gmail.com>
2019-02-20 00:00:54 +0700

    Unfinished word2vec

--- model/Word2VEc.py
+++ model/Word2VEc.py
@@ -8,6 +8,7 @@ import os
 import zipfile
 import numpy as np
 import tensorflow as tf
+import json
 import keras
 from urllib import request
 
@@ -66,97 +67,116 @@ def collect_data(vocabulary_size=10000):
     return data, count, dictionary, reverse_dictionary
 
 
-vocab_size = 10000
-data, count, dictionary, reverse_dictionary = collect_data(vocabulary_size=vocab_size)
-print(data[:7])
-
-window_size = 3
-vector_dim = 300
-epochs = 110100
-valid_size = 16  # Random set of words to evaluate similarity on.
-valid_window = 100  # Only pick dev samples in the head of the distribution.
-valid_examples = np.random.choice(valid_window, valid_size, replace=False)
-
-sampling_table = keras.preprocessing.sequence.make_sampling_table(vocab_size)
-couples, labels = keras.preprocessing.sequence.skipgrams(data, vocab_size, window_size=window_size,
-                                                         sampling_table=sampling_table)
-word_target, word_context = zip(*couples)
-word_target = np.array(word_target, dtype="int32")
-word_context = np.array(word_context, dtype="int32")
-
-print(couples[:10], labels[:10])
-
-# create some input variables
-input_target = Input((1,))
-input_context = Input((1,))
-
-embedding = Embedding(vocab_size, vector_dim, input_length=1, name='embedding')
-target = embedding(input_target)
-target = Reshape((vector_dim, 1))(target)
-context = embedding(input_context)
-context = Reshape((vector_dim, 1))(context)
-
-# setup a cosine similarity operation which will be output in a secondary model
-similarity = merge.dot(inputs = [target, context], normalize=True, axes=0)
-# similarity = merge([target, context], mode='cos', dot_axes=0)
-
-# now perform the dot product operation to get a similarity measure
-# dot_product = merge.Dot(normalize=False, axes=1)
-dot_product = merge.dot(inputs = [target, context], axes = 1, normalize=False)
-dot_product = Reshape((1,))(dot_product)
-# add the sigmoid output layer
-output = Dense(1, activation='sigmoid')(dot_product)
-# create the primary training model
-# model = Model(input_shape=[input_target, input_context], output=output)
-model = Model(input=[input_target, input_context], output=output)
-model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(lr=0.001))
-
-# create a secondary validation model to run our similarity checks during training
-# validation_model = Model(input_shape=[input_target, input_context], output=similarity)
-validation_model = Model(input=[input_target, input_context], output=similarity)
+# function to test model
+def test_model(data_path):
+    model = keras.models.load_model("C:/Users/hpEnvy/back-end/model/word2vec_model.h5")
+    reviews = []
+    for file in os.listdir(data_path):
+        with open(data_path+file, 'r') as fp:
+            data = json.load(fp)
+            fp.close()
+            
+            for review in data['Reviews']:
+                reviews.append(review)
+    tokenizer = keras.preprocessing.text.Tokenizer()
+    tokenizer.fit_on_texts(reviews)
+    
+                
 
 
-class SimilarityCallback:
-    def run_sim(self):
-        for i in range(valid_size):
-            valid_word = reverse_dictionary[valid_examples[i]]
-            top_k = 8  # number of nearest neighbors
-            sim = self._get_sim(valid_examples[i])
-            nearest = (-sim).argsort()[1:top_k + 1]
-            log_str = 'Nearest to %s:' % valid_word
-            for k in range(top_k):
-                close_word = reverse_dictionary[nearest[k]]
-                log_str = '%s %s,' % (log_str, close_word)
-            print(log_str)
+test_model("C:/Users/hpEnvy/Downloads/crime-20190219T153822Z-001/crime/")
 
-    @staticmethod
-    def _get_sim(valid_word_idx):
-        sim = np.zeros((vocab_size,))
-        in_arr1 = np.zeros((1,))
-        in_arr2 = np.zeros((1,))
-        in_arr1[0,] = valid_word_idx
-        for i in range(vocab_size):
-            in_arr2[0,] = i
-            out = validation_model.predict_on_batch([in_arr1, in_arr2])
-            sim[i] = out
-        return sim
+# vocab_size = 10000
+# data, count, dictionary, reverse_dictionary = collect_data(vocabulary_size=vocab_size)
+# print(data[:7])
+
+# window_size = 3
+# vector_dim = 300
+# epochs = 110100
+# valid_size = 16  # Random set of words to evaluate similarity on.
+# valid_window = 100  # Only pick dev samples in the head of the distribution.
+# valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+
+# sampling_table = keras.preprocessing.sequence.make_sampling_table(vocab_size)
+# couples, labels = keras.preprocessing.sequence.skipgrams(data, vocab_size, window_size=window_size,
+#                                                          sampling_table=sampling_table)
+# word_target, word_context = zip(*couples)
+# word_target = np.array(word_target, dtype="int32")
+# word_context = np.array(word_context, dtype="int32")
+
+# print(couples[:10], labels[:10])
+
+# # create some input variables
+# input_target = Input((1,))
+# input_context = Input((1,))
+
+# embedding = Embedding(vocab_size, vector_dim, input_length=1, name='embedding')
+# target = embedding(input_target)
+# target = Reshape((vector_dim, 1))(target)
+# context = embedding(input_context)
+# context = Reshape((vector_dim, 1))(context)
+
+# # setup a cosine similarity operation which will be output in a secondary model
+# similarity = merge.dot(inputs = [target, context], normalize=True, axes=0)
+# # similarity = merge([target, context], mode='cos', dot_axes=0)
+
+# # now perform the dot product operation to get a similarity measure
+# # dot_product = merge.Dot(normalize=False, axes=1)
+# dot_product = merge.dot(inputs = [target, context], axes = 1, normalize=False)
+# dot_product = Reshape((1,))(dot_product)
+# # add the sigmoid output layer
+# output = Dense(1, activation='sigmoid')(dot_product)
+# # create the primary training model
+# # model = Model(input_shape=[input_target, input_context], output=output)
+# model = Model(input=[input_target, input_context], output=output)
+# model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(lr=0.001))
+
+# # create a secondary validation model to run our similarity checks during training
+# # validation_model = Model(input_shape=[input_target, input_context], output=similarity)
+# validation_model = Model(input=[input_target, input_context], output=similarity)
 
 
-sim_cb = SimilarityCallback()
+# class SimilarityCallback:
+#     def run_sim(self):
+#         for i in range(valid_size):
+#             valid_word = reverse_dictionary[valid_examples[i]]
+#             top_k = 8  # number of nearest neighbors
+#             sim = self._get_sim(valid_examples[i])
+#             nearest = (-sim).argsort()[1:top_k + 1]
+#             log_str = 'Nearest to %s:' % valid_word
+#             for k in range(top_k):
+#                 close_word = reverse_dictionary[nearest[k]]
+#                 log_str = '%s %s,' % (log_str, close_word)
+#             print(log_str)
 
-arr_1 = np.zeros((1,))
-arr_2 = np.zeros((1,))
-arr_3 = np.zeros((1,))
-for cnt in range(epochs):
-    idx = np.random.randint(0, len(labels)-1)
-    arr_1[0,] = word_target[idx]
-    arr_2[0,] = word_context[idx]
-    arr_3[0,] = labels[idx]
-    loss = model.train_on_batch([arr_1, arr_2], arr_3)
-    if cnt % 100 == 0:
-        print("Iteration {}, loss={}".format(cnt, loss))
-    if cnt % 10000 == 0:
-        sim_cb.run_sim()
+#     @staticmethod
+#     def _get_sim(valid_word_idx):
+#         sim = np.zeros((vocab_size,))
+#         in_arr1 = np.zeros((1,))
+#         in_arr2 = np.zeros((1,))
+#         in_arr1[0,] = valid_word_idx
+#         for i in range(vocab_size):
+#             in_arr2[0,] = i
+#             out = validation_model.predict_on_batch([in_arr1, in_arr2])
+#             sim[i] = out
+#         return sim
 
-model.save(filepath="word2vec_model.h5")
-model.save_weights(filepath="word2vec_weight.h5")
+
+# sim_cb = SimilarityCallback()
+
+# arr_1 = np.zeros((1,))
+# arr_2 = np.zeros((1,))
+# arr_3 = np.zeros((1,))
+# for cnt in range(epochs):
+#     idx = np.random.randint(0, len(labels)-1)
+#     arr_1[0,] = word_target[idx]
+#     arr_2[0,] = word_context[idx]
+#     arr_3[0,] = labels[idx]
+#     loss = model.train_on_batch([arr_1, arr_2], arr_3)
+#     if cnt % 100 == 0:
+#         print("Iteration {}, loss={}".format(cnt, loss))
+#     if cnt % 10000 == 0:
+#         sim_cb.run_sim()
+
+# model.save(filepath="word2vec_model.h5")
+# model.save_weights(filepath="word2vec_weight.h5")
