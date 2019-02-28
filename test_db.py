f5c1ace5f0322d6a35ea8a4bbb0bab81b67c4a1c
jawjab <jibbizjab@gmail.com>
2019-02-28 18:46:29 +0700

    modified api for get books

--- test_db.py
+++ test_db.py
@@ -1,14 +1,35 @@
 import sys
+import json
+import os
 import pymongo
-
+from pprint import pprint
+import time
 uri = "mongodb://tsundoku_db:tsundoku_db_007@ds133875.mlab.com:33875/tsundoku"
 
 client = pymongo.MongoClient(uri)
 
 db = client.get_default_database()
 
-users = db['Users']
+books = db['Books']
+
+# start = time.time()
+# query = books.find({"Genre":"/^{}$/i".format("crime")}).limit(50)
+# que_time = time.time()
+# for doc in query:
+#     pprint(doc)
+# print(start-que_time)
 
 
-print(users.find_one({"name":"test"}))
-print("test")
+genres = os.listdir("web_scraper/goodreads/novel/")
+
+for genre in genres:
+    books_list = os.listdir("web_scraper/goodreads/novel/{}/".format(genre))
+    for book in books_list:
+        if book != ".DS_Store":
+            with open("web_scraper/goodreads/novel/{}/{}".format(genre, book), "r") as fp:
+                data = json.load(fp)
+                data["_id"] = data["ID"]
+            try:
+                books.insert(data)
+            except:
+                continue
