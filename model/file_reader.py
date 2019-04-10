3dcfb213af0c9c75eb27e172a88c25e99a774c2d
jawjab <jibbizjab@gmail.com>
2019-04-10 18:36:08 +0700

    new read v2

--- model/file_reader.py
+++ model/file_reader.py
@@ -14,16 +14,24 @@ class file_reader:
 
         return data, label
     
-    def read_v2(self, path):
+    def read_v2(self, path, position, num_of_lab):
         with open(path, "r", encoding='utf-8') as fp:
             data = []
             label = []
+            
             for line in fp.readlines():
-                if re.search("\,\s[\-]?[0-2]$", line):
+                if re.search("\,\s[\-]?[\d]$", line):
                     sentence_elements = line.split(",")
-                    if int(sentence_elements[-2].replace("\n", "").strip()) != 0:
-                        data.append(",".join(sentence_elements[0: -2]))
-                        label.append(int(sentence_elements[-1].replace("\n", "").strip()))
+                    checker = sentence_elements[:]
+                    checker.reverse()
+                    count = 0
+                    for i in range(0, len(checker)):
+                        if type(checker[i]) is int:
+                            count += 1
+                    if count == num_of_lab:
+                        if int(sentence_elements[(-1*position)].replace("\n", "").strip()) != 0:
+                            data.append(",".join(sentence_elements[0: (-1*position)]))
+                            label.append(int(sentence_elements[-1].replace("\n", "").strip()))
         
         return data, label
 
