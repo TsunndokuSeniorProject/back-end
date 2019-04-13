2cf806d64edfee0bd05bdc6db6960ac7f0a4b8db
jawjab <jibbizjab@gmail.com>
2019-04-13 15:37:45 +0700

    remove spacy download

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ import pymongo
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-import subprocess
-subprocess.run(["python -m spacy download en_core_web_sm"])
 
 app = Flask(__name__)
 
