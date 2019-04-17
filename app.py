ad4c83a4c95505f9df2be404be89bc277ef8de38
jawjab <jibbizjab@gmail.com>
2019-04-17 21:23:01 +0700

    performance tuning

--- app.py
+++ app.py
@@ -102,20 +102,19 @@ def get_review_by_id_with_predict_result(id):
         for review in book_reviews['Reviews']:
             text += review['Review'] + " "
 
-        text = text_processor.replace_author(text, book_reviews["Author"])
-        text = text_processor.replace_bookname(text, book_reviews["Name"])
+        # text = text_processor.replace_author(text, book_reviews["Author"])
+        # text = text_processor.replace_bookname(text, book_reviews["Name"])
         sentences_list = text_processor.split_into_sentences(text)
-        sentences_list = text_processor.filter_english(sentences_list)
-        # global aspect_res
-        # aspect_res = aspect_gensim.predict(sentences_list)
-        # global graph, polarity_lstm
-        # with graph.as_default():
-        #     result = np.asarray(polarity_lstm.predict(sentences_list))
-        # result = find_max(result)
-        # result = pd.DataFrame({"sentences": sentences_list, "aspect": aspect_res, "polarity": result})
-        # result = pd.DataFrame({"sentences": sentences_list})
-        # result = result.to_dict("records")
-        book_reviews['sentiment'] = sentences_list#result
+        # sentences_list = text_processor.filter_english(sentences_list)
+        global aspect_res
+        aspect_res = aspect_gensim.predict(sentences_list)
+        global graph, polarity_lstm
+        with graph.as_default():
+            result = np.asarray(polarity_lstm.predict(sentences_list))
+        result = find_max(result)
+        result = pd.DataFrame({"sentences": sentences_list, "aspect": aspect_res, "polarity": result})
+        result = result.to_dict("records")
+        book_reviews['sentiment'] = result
         return jsonify(book_reviews)
     return jsonify({"fail_message":"couldn't find book by the given id."})
 
@@ -171,6 +170,6 @@ if __name__=="__main__":
     
     
 
-    # port = int(os.environ.get('PORT', 33507))
-    port = 8000
+    port = int(os.environ.get('PORT', 33507))
+    # port = 8000
     app.run(debug=True, port=port)
