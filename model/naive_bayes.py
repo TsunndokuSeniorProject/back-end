3f766b4fa7d305e985136770071d5f38e762e74d
kkamalan <kkamalan@stud.hs-offenburg.de>
2019-04-19 20:14:17 +0700

    naive baeys

--- model/naive_bayes.py
+++ model/naive_bayes.py
@@ -3,21 +3,48 @@ import os
 import json
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, ComplementNB
 import re
 import csv
 from file_reader import file_reader
 import random
 
 
+# with open("C:/Users/USER/Downloads/sentence-polarity/sentence_polarity/rt-polarity-pos.txt", 'r', encoding='utf8') as fp:
+#     pos = fp.readlines()
+# pos_label = [ 1 for x in pos]
+# with open("C:/Users/USER/Downloads/sentence-polarity/sentence_polarity/rt-polarity-neg.txt", 'r', encoding='utf8') as fp:
+#     neg = fp.readlines()
+# neg_label = [ 2 for x in pos]
 
-reviews, labels = file_reader().read_v2("C:/Users/USER/Downloads/test.txt", 1, 2)
+# train_set = pos + neg
+# train_labels = pos_label + neg_label
 
-train_set = reviews[0:701]
-train_labels = labels[0:701]
+reviews, labels = file_reader().read_v2("C:/Users/USER/Downloads/test.txt", 2, 2)
+print(labels)
+train_set = []
+train_labels = []
+X_reviews = reviews[:1600]
+X_labels = labels[:1600]
 
-test_set = reviews[701:750]
-test_labels = labels[701:750]
+count = 0
+for x, y in zip(X_reviews, X_labels):
+    if y in [0,4,5,6]:
+        continue
+    elif y == 1:
+        if count == 5:
+            count = 0
+            continue
+        else:
+            count += 1
+            train_set.append(x)
+            train_labels.append(y)
+    else:
+        train_set.append(x)
+        train_labels.append(y)
+
+raw_test_set = reviews[1601:]
+raw_test_labels = labels[1601:]
 # with open(directory, "r", encoding="utf-8") as fp:
 #     data = fp.readlines()
 
@@ -41,45 +68,45 @@ test_labels = labels[701:750]
 #         reviews.append(temp[0])
 #         labels.append(temp[1])
 
-# wtf_zeros = random.sample(zeros, 80)
-
-# ok_labels = []
-# for smth in labels:
-#     ok_labels.append(smth.replace("\n", "").replace(" ", ""))
-
-# for smth in wtf_zeros:
-#     ok_labels.append('0')
-
-# reviews += wtf_zeros
 count = {}
-for single in labels:
+for single in train_labels:
     if single in count:
         count[single] += 1
     else:
         count[single] = 1
-
+print(count)
 count_vec = CountVectorizer()
 
 xtrain = count_vec.fit_transform(train_set)
 tfidf_transformer = TfidfTransformer()
 xtrain_tfidf = tfidf_transformer.fit_transform(xtrain)
 
-clf = MultinomialNB().fit(xtrain_tfidf, train_labels)
-# test_string = ["I hate Harry Potter", "The story is very rich", "Vaguely headachey, I needed a reading distraction, and the appropriate story in these kinds of situations is a touchy one", "The character sucks, I don't enjoy the protagonist at all"]
-# test_string.append("Probably the worst part is struggling through all the rampant racism, which isn't nearly as funny as the rampant anti-Mormonism was in aSiS")
-# test_string.append("The writing by Agartha Christie is superb, the pacing was great")
-# test_string.append("the fact that the cover is the most evocative thing about a novel that should have had atmosphere to die for made me feel like I was dying inside each time I turned the page only to discover 100% plot mechanics and 0% anything of interest besides the, I suppose, \"page-turning\" plot.")
-# test_string.append("The emphasis in this well-intentioned advice by Mrs. Fairfax is on the word MARRY.")
-# test_string.append("Anna is so bitchy i wanna kill her")
-# test_string.append("Is she so terrified of breaking cultural norms and coming across as a mean-crazy-angry-dyke-shrill-frigid bitch")
-# test_string.append("How could this be a work of a professional writer? The book was awful to read")
-# test = count_vec.transform(test_string)
-# test = tfidf_transformer.transform(test)
+clf = ComplementNB().fit(xtrain_tfidf, train_labels)
 
-# with open(test_direc, 'r', encoding='utf-8') as fp:
-#     test_data = fp.readlines()
-# for sen in test_data:
-#     test_set.append(sen)
+
+# test
+# raw_test_set, raw_test_labels = file_reader().read_v2("C:/Users/USER/Downloads/test.txt", 1, 2)
+test_set = []
+test_labels = []
+for a, b in zip(raw_test_set, raw_test_labels):
+    if b == 0:
+        continue
+    else:
+        test_set.append(a)
+        test_labels.append(b)
+    # test_set.append(a)
+    # test_labels.append(b)
+
+count = {}
+for single in test_labels:
+    if single in count:
+        count[single] += 1
+    else:
+        count[single] = 1
+print(count)
+
+
+
 
 test_X = count_vec.transform(test_set)
 test_X = tfidf_transformer.transform(test_X)
@@ -110,4 +137,4 @@ for single in result:
         count[single] = 1
 # with open('bayes_result.txt', 'w', encoding='utf-8') as fp:
 #     fp.write(ready_for_print)
-# print(count)
+print(count)
